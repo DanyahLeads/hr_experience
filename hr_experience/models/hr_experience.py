6f46e4cbc3bc8f5ea2f91c56f6c75523b8373334
danyah <dania.hussein@leading-point.com>
2023-04-18 12:34:48 +0300

    add activate botton

--- hr_experience/models/hr_experience.py
+++ hr_experience/models/hr_experience.py
@@ -3,18 +3,19 @@ from odoo import models, fields, api
 
 class hrExperience(models.Model):
     _name = 'hr.experience'
-    _description = "Hiring the Heros!"
+    _description = "Hiring the Heroes!"
     name = fields.Char("Name")
     date_to = fields.Date("Date To", required='1')
     date_from = fields.Date("Date From", required='1')
     education = fields.Boolean("Is Educated")
     college = fields.Char("The College/Company", help="from where you gained this experience")
-    state = fields.Selection(string='State', selection=[('active', 'Active'), ('expired', 'Expired')])  # default=active
+    state = fields.Selection(string='State', selection=[('active', 'Active'), ('expired', 'Expired')],
+                             readonly='1')  # default=active
     document = fields.Binary("Documents")
     description = fields.Text("Description")
     employee_id = fields.Many2one('hr.employee', string='Employee ID')
-    contract_id = fields.Char(compute="_compute_contract_id", string='Contract', store=True)
-    expiration_date = fields.Date(string='Expiration Date', compute="_compute_expiration_date", store=True)
+    contract_id = fields.Char('Contract', readonly=True)
+    expiration_date = fields.Date(string='Expiration Date')
 
     def name_get(self):
         rec = []
@@ -28,35 +29,31 @@ class hrExperience(models.Model):
         rec['employee_id'] = self._context.get('active_id')
         return rec
 
-    @api.constrains('date_from')
+    @api.onchange('expiration_date')
+    def onchange_state(self):
+        if self.expiration_date <= fields.Date.today():
+            self.state = 'expired'
+        else:
+            self.state = 'active'
+
+    @api.onchange('employee_id')
+    def onchange_contract_id(self):
+        self.contract_id = self.employee_id.contract_id.name
+
+    @api.constrains('date_from', 'date_to', 'expiration_date', 'state')
     def _check_date_from(self):
         for record in self:
-            if record.date_from > record.date_to:
-                raise models.ValidationError('Date from must not be after Date to!')
-            if record.date_from > fields.Date.today():
+            if record.date_from > record.date_to or record.date_from > fields.Date.today():
+                raise models.ValidationError('Date from must be in the past and must not be after Date to!')
+            elif record.date_from == fields.Date.today() or record.date_from == record.date_to:
                 raise models.ValidationError('Date from must be in the past!')
-            if record.date_from == fields.Date.today():
-                raise models.ValidationError('Date from must be in the past!')
-
-    @api.constrains('date_to')
-    def _check_date_to(self):
-        for record in self:
-            if record.date_to > fields.Date.today():
-                raise models.ValidationError('Date to must be in the past!')
-            if record.date_to == fields.Date.today():
+            elif record.date_to > fields.Date.today() or record.date_to == fields.Date.today():
                 raise models.ValidationError('Date to must be in the past!')
 
-    @api.depends('state')
-    def _compute_expiration_date(self):
-        for record in self:
-            if record.state == 'expired':
-                record.expiration_date = fields.Date.today()
-
-    @api.depends('employee_id')
-    def _compute_contract_id(self):
-        for record in self:
-            if record.employee_id.contract_id != False:
-                record.contract_id = record.employee_id.contract_id.name
+    def action_activate(self):
+        for rec in self:
+                rec.expiration_date = False
+                rec.state = 'active'
 
 
 class Experiences(models.Model):
